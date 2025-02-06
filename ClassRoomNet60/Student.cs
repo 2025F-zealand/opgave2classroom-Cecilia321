08a7d2c77ac7d921bbcdf0e517f41f3a4dbc222f
Cecilia Liv Starck <cecilia-liv@hotmail.com>
2025-02-06 13:09:57 +0100

    Opgave 1 klass student, proporties og konstruktor

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    internal class Student
+    {
+        private string name { get; }
+        private int birthMonth { get;}
+        private int birthDay { get;}
+
+
+        public Student(string name, int birthMonth, int birthDay)
+        {
+            this.name = name;
+            this.birthMonth = birthMonth;
+            this.birthDay = birthDay;
+        }
+    }
+}
