cc1e32157eec2afad57e83ec48bdd1fd556e3313
Cecilia Liv Starck <cecilia-liv@hotmail.com>
2025-02-06 13:40:40 +0100

    opgave 4. oprettet nyt instans af classroom i main

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ClassRoomNet60;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //instalisier et nyt instans af klassen Classroom
+        ClassRoom classRoom = new ClassRoom(); 
+        classRoom.ClassName = "3Q";
+        classRoom.SemesterStart = new DateTime(2019, 8, 26);
+        classRoom.StudentList = new List<Student>();
+
+        classRoom.StudentList.Add(new Student("Cecilia", 03, 14));
+        classRoom.StudentList.Add(new Student("rebekka", 04, 13));
+        classRoom.StudentList.Add(new Student("Sarah", 01, 07));
+
+        Console.WriteLine($"Class Name: {classRoom.ClassName}");
+        Console.WriteLine($"Semester Start: {classRoom.SemesterStart.ToShortDateString()}");
+        Console.WriteLine("Students:");
+
+        foreach (var student in classRoom.StudentList)
+        {
+            Console.WriteLine($"- {student.Name}");
+        }
+
+    }
+}
